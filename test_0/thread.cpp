5442ed3ee57f62f25eda7fb55ba3298d7e34b482
GREEK_VICTOR <PastoreMaxwell@outlook.com>
2019-01-09 23:17:43 +0800

    稍作修改

--- test_0/thread.cpp
+++ test_0/thread.cpp
@@ -10,11 +10,13 @@ std::timed_mutex mtx;
 void myfirstThread(int num)
 {
 	while (mtx.try_lock_for(std::chrono::microseconds(200))) {
-	for (int i = 0; i < 10; ++i) {
-		
-			cout << "Hello world="<<num++<<"次" << endl;
-			cout << endl;
+		for (int i = 0; i < 10; ++i) {
+
+			//cout << "Hello world="<<num++<<"次" << endl;
+			//cout << endl;
+			printf("Hello world %i 次。\n", num);
 			this_thread::sleep_for(chrono::milliseconds(10));
+			num++;
 		}
 	}
 	this_thread::sleep_for(chrono::milliseconds(10));
@@ -23,28 +25,31 @@ void myfirstThread(int num)
 void mysecondThread(int &num)
 {
 	while (!mtx.try_lock_for(std::chrono::microseconds(200))) {
-	this_thread::sleep_for(std::chrono::microseconds(10));
-	for (int i = 0; i < 10; ++i) {
-		cout << "Fuck you"<<num++ <<"次"<< endl;
-		cout << endl;
+		this_thread::sleep_for(std::chrono::microseconds(10));
+		for (int i = 0; i < 10; ++i) {
+			//cout << "Fuck you" << num++ << "次" << endl;
+			//cout << endl;
+			printf("Fuck you %i 次。\n", num);
+			this_thread::sleep_for(chrono::milliseconds(10));
+			num++;
+		}
 	}
-}
 	this_thread::sleep_for(std::chrono::microseconds(10));
 	mtx.unlock();
 }
 
-int _tmain(int argc,_TCHAR* argv[])
+int _tmain(int argc, _TCHAR* argv[])
 {
-	int n = 0;
+	int n = 1;
 	thread myThread0;
 
-	thread myThread1(myfirstThread,n);
-	thread yourThread(mysecondThread,std::ref(n));
+	thread myThread1(myfirstThread, n);
+	thread yourThread(mysecondThread, std::ref(n));
 	myThread1.join();
 	yourThread.join();
 
 	n++;
-
+	//printf("\nmove!\n");
 	thread myThread3(std::move(yourThread));
 	return system("pause");
 }
